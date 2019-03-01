43b5a60cae9ef94bfb3123b1fd6a653cbf5549b5
Callan <cjansen4@asu.edu>
2019-03-01 03:50:34 -0700

    Added decryption function to return the original message.

--- CaeserCipher.py
+++ CaeserCipher.py
@@ -4,12 +4,12 @@ try:
 except ValueError:
     print("Invalid Input - enter a String")
 
-print(message_input)
-
 try:
     key_input = int(input("Enter a key from 1-25: "))
 except NameError:
     print("Invalid Input - enter an int")
+
+print(message_input)
 
 
 def caeser_cipher(message, key):
@@ -22,12 +22,28 @@ def caeser_cipher(message, key):
             encrypted += chr(num_mod + starting_alpha)
         else:
             encrypted += character
-    print(encrypted)
+    return encrypted
 
 
-caeser_cipher(message_input, key_input)
+encrypted_message = caeser_cipher(message_input, key_input)
+print(encrypted_message)
 
 
+def caeser_decryption(message, key):
+    decrypted = ""
+    starting_alpha = ord('A')
+    for character in message:
+        if character.isalpha():
+            num = ord(character) - starting_alpha
+            num_mod = (num - key) % 26
+            decrypted += chr(num_mod + starting_alpha)
+        else:
+            decrypted += character
+    return decrypted
+
+
+decrypted_message = caeser_decryption(encrypted_message, key_input)
+print(decrypted_message)
 
 
 
